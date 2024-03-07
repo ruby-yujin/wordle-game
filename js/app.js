59d922fb4c753e75835827f267010b1c7e1eb7d8
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-03-07 18:46:58 +0900

    [feature] js  시작
    - app.js 생성
    - data-index 0부터로 바꿈

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function appStart() {
+  const handleKeydown = (event) => {
+    const key = event.key;
+    const keyCode = event.keyCode;
+  };
+  //로직
+  window.addEventListener("keydown", handleKeydown);
+}
+
+appStart();
